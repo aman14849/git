7615a339f61ca326721174d306bb002c1ab567f3
aman_singh <aman14849@gmail.com>
2023-11-09 22:50:11 +0530

    initial commit

---
+++ src/main/java/com/git/demo/git/controller/GitController.java
@@ -0,0 +1,18 @@
+package com.git.demo.git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("git")
+public class GitController {
+
+	@GetMapping("/status")
+	public String checkApplicatoin() {
+		return "running!";
+	}
+	
+	
+	
+}
